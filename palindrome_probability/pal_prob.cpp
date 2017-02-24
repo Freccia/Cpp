54ea6ffd8474dd725f56239acaf3871194517d6f
Freccia <Freccia@users.noreply.github.com>
2017-02-24 17:26:43 +0100

    palindrome probability OK

--- palindrome_probability/pal_prob.cpp
+++ palindrome_probability/pal_prob.cpp
@@ -10,62 +10,120 @@
 #include <iostream>
 #include <string>
 
-class palindromeProbability
+class palProb
 {
 	private:
 
-		int			ft_strlen(char *str)
+		void		setAlfa()
 		{
-			int		len = 0;
-			int		i	= -1;
+			int		i = -1;
 
-			while (str[++1])
-				++len;
-			return (len);
+			while (++i < 26)
+				alfa[i] = 0;
 		}
 
-		int			numPalindromes(char *str)
+		int			checkOnePivot()
 		{
-			int		npal = 0;
+			int		i = -1;
+			int		pivot = 0;
 
-			return npal;
+			while (++i < 26)
+			{
+				if (alfa[i] % 2)
+				{
+					if (pivot)
+						return (1);
+					++pivot;
+				}
+//				std::cout << "alfa[" << i << "]: " << (char)(i + 'a') << " " << alfa[i] << std::endl;
+			}
+			return (0);
 		}
-		
-		int			numAnagrams(char *str)
-		{
-			int		nan = 0;
 
+		long		factor(long n)
+		{
+			int		k = n;
+
+			while (--k > 1)
+			{
+				n *= k;
+			}
+			return (n);
+		}
+
+		void		countLetters(std::string str)
+		{
+			int		i = -1;
+			int		len = str.length();
+
+			while (++i < len)
+			{
+				if (str[i] >= 'A' && str[i] <= 'Z')
+					str[i] += 32;
+				++alfa[str[i] - 'a'];
+//				std::cout << "alfa[" << str[i] - 'a' << "]: " << str[i] << " " << alfa[i] << std::endl << std::endl;
+			}
+		}
+
+		long		getNumAnagrams(std::string str)
+		{
+			int		i = -1;
+			int		len = str.length();
+			long	nan = factor(len);
+
+			while (++i < 26)
+			{
+				if (alfa[i] > 1)
+					nan /= factor(alfa[i]);
+			}
 			return nan;
 		}
 
-		char		**getAnagrams(char *str)
+		long		getNumPalindromes(std::string str)
 		{
-			char	**tab;
-			int		i = -1;
+			int		len = str.length();
+			long	npal;
 
-			if ((tab = (char**)malloc(sizeof(tab) * nan)) == NULL)
-				return NULL;
-			while (++i < nan)
-			{
-				tab[i] = ;
-			}
-			return tab;
+			npal = factor(len / 2);
+			return npal;
 		}
 
 	public:
 
-		int		nan   = numAnagrams(str);
-		int		len   = ft_strlen(str);
-		char	**tab = getAnagrams(str);
+		int		alfa[26] = {0};
 
-		double		getProb(std::string<char> str)
+		float		getProb(std::string str)
 		{
-			int		npal = numPalindromes(str);
-			double	ret	 = 0;
+			float	ret = 0;
 
-			if (nan && npal)
-				ret = npal / nan;
+			if (str.empty())
+				return (0);
+			setAlfa();
+			countLetters(str);
+			if (checkOnePivot())
+				return (0);
 
+			long	nan = getNumAnagrams(str);
+			long	npal = getNumPalindromes(str);
+
+//			std::cout << "nan: " << nan << std::endl;
+//			std::cout << "npal: " << npal << std::endl;
+			if (nan != 0)
+				ret = (float)npal / (float)nan;
+			
 			return ret;
 		}
+};
+
+
+int			main(int ac, char **av)
+{
+	palProb	pB;
+	int		i = 0;
+
+	while (++i < ac)
+	{
+		std::cout << "Probability of  " << av[i] << ":" << std::endl << pB.getProb(av[i]) << std::endl;
+	}
+	return (0);
 }
